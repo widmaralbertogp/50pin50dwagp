a84e59a21d03832d2cae3db10fd7e501c28c23e1
Widmar Alberto Gómez Pérez <widmar.alberto.gp@gmail.com>
2025-08-13 00:12:57 -0500

    Commit 12Aug25 50pin50d

---
+++ projects/day1/script.js
@@ -0,0 +1,15 @@
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasess()
+        panel.classList.add('active')
+    })
+    
+})
+
+function removeActiveClasess(){
+    panels.forEach(panel =>{
+        panel.classList.remove('active')
+    })
+}
