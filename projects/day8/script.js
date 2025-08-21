d0e4b380c2179831764011e4116e323a512dd049
Widmar Alberto Gómez Pérez <widmar.alberto.gp@gmail.com>
2025-08-21 02:54:30 -0500

    Commit 21Agu 252 50pin50d

--- projects/day8/script.js
+++ projects/day8/script.js
@@ -3,5 +3,6 @@ const labels = document.querySelectorAll('.form-control label')
 labels.forEach(label => {
     label.innerHTML = label.innerText
         .split('')
-        .map( () => ``)
+        .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
+        .join('')
 }) 
